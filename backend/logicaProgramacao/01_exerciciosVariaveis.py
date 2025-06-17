857cf7aa384c82abac12545200301cf38c5d65bc
vitoria pineli <vitoriapineli121@gmail.com>
2025-06-17 09:46:53 -0300

    Trabalhando com variaveis

---
+++ backend/logicaProgramacao/01_exerciciosVariaveis.py
@@ -0,0 +1,19 @@
+cachorro = "jack"
+idade = 5
+altura = 0.50
+tem_carteira_vacina = True
+
+print("cachorro:", cachorro)
+print("Idade:", idade)
+print("Altura:", altura)
+print("tem_carteira_vacina?", tem_carteira_vacina)
+
+gato = "nina"
+idade = 6
+altura = 0.60
+tem_carteira_vacina = True
+
+print("gato" , gato)
+print("Idade:", idade)
+print("Altura:", altura)
+print("tem_carteira_vacina?", tem_carteira_vacina)
---
+++ backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,9 @@
+nome = "vitoria"
+idade = 17
+altura = 1.59
+tem_carteira = False
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Tem carteira?", tem_carteira)
