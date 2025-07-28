35fac6a50cdec5ade369622bd814fc3e236a0176
vitoria <vitoriapineli121@gmail.com>
2025-07-28 08:54:27 -0300

    laços

---
+++ backend/logicaProgramacao/07_exerciciosLaços.py
@@ -0,0 +1,11 @@
+soma = 0 
+for i in range (21):
+    soma += i
+
+# soma = soma + i 
+print ("soma dos valores" , soma)
+
+
+     ATIVIDADE 
+# 1- MOSTRAR SOMA DOS NUMEROS DE 1 A 20 (FOR)
+# 2- pedir ao usuario valores e soma-los enquanto o usuario nao digitar erro 
