70e328357e969c2e4db641c3f9d952754c29f53a
vitoria pineli <vitoriapineli121@gmail.com>
2025-09-09 11:45:04 -0300

    trabalho em arquivos

--- backend/logicaProgramacao/20_ExerxiciciosAvaliativosArquivos.py
+++ backend/logicaProgramacao/20_ExerxiciciosAvaliativosArquivos.py
@@ -26,19 +26,24 @@ def menu():
         
         if opcao == "1":
             cadastro()
-    
+            menu()
+
         elif opcao == "2":
             lista() 
+            menu()
 
         elif opcao == "3":   
             alterar_produto()
+            menu()
 
-        elif opcap == "4":
+        elif opcao == "4":
             excluir()
+            menu()
 
         elif opcao == "sair" :
             print("saindo...")   
-    
+            break   
+
 
 def cadastro():   
     lista = []
@@ -47,7 +52,7 @@ def cadastro():
 
     with open('padaria.txt', 'w') as arquivo: 
         arquivo.writelines(lista)
-        break
+        
 
 def lista():
     lista = []
@@ -57,38 +62,41 @@ def lista():
     
 
 def alterar_produto():
-    lista = []
-    produto_alterar = input("Digite o nome do produto para alterar:")
-    with open('padaria.txt', 'r') as arquivo: 
-            lista = arquivo.readlines()
+    produto_alterar = input("Digite o nome do produto para alterar:\n").strip()
+    
+    with open('padaria.txt', 'r') as arquivo:
+        lista = arquivo.readlines()
 
-    for produto in lista:
-        if produto == produto_alterar:
-            novo_nome = input("Digite o nome alterado: ")
-            produto = novo_nome
+    nova_lista = []
 
-    with open('padaria.txt', 'w') as arquivo: 
-        arquivo.writelines(lista)
+    for linha in lista:
+        if linha.strip() == produto_alterar:
+            novo_nome = input("Digite o novo nome do produto:\n").strip()
+            nova_lista.append(novo_nome )
+
+    with open('padaria.txt', 'w') as arquivo:
+        arquivo.writelines(nova_lista)
+        print("Produto alterado com sucesso!")
+    
     
 def excluir():
-    lista = []
-    produto_excluir = input("Digite o nome do produto para excluir:")
-    with open('padaria.txt', 'r') as arquivo: 
-            lista = arquivo.readlines()
+    produto_excluir = input("Digite o nome do produto para excluir:\n").strip()
+    
+    with open('padaria.txt', 'r') as arquivo:
+        lista = arquivo.readlines()
 
-    for produto in lista:
-        if produto != produto_excluir:
-            lista.append(produto)
+    nova_lista = []
 
-    with open('padaria.txt', 'w') as arquivo: 
-        arquivo.writelines(lista)
+    for linha in lista:
+        if linha.strip() != produto_excluir:
+            nova_lista.append(linha)
+
+    with open('padaria.txt', 'w') as arquivo:
+        arquivo.writelines(nova_lista)
+        print("Produto excluído com sucesso!")
+    
 
 menu()
-
-
-
-   
-
 
 
 with open('arquivo.txt', 'r') as file: dados = file.readlines()
@@ -96,11 +104,109 @@ with open('arquivo.txt', 'r') as file: dados = file.readlines()
 
 
 
-
-
     
                               #EXERCICIO 2
 #Sou dono de uma concessionaria e vi o sistema do dono da padaria. Gostaria de um sistema igual para meus carros.
+
+def carregarArquivo():
+    try:
+        with open('concessionaria.txt', 'r') as arquivo: 
+            lista = arquivo.readlines()
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+
+def menu():
+    carregarArquivo()
+    print("=== Menu concessionaria === \n")
+    print("1 -cadastro \n")
+    print("2 -lista \n")
+    print("3 - alterar produto \n")
+    print("4 - excluir produto \n")
+    print("5 - sair")
+    opcao = input ("digite a opcao desejada: ")
+
+
+    while opcao != "sair":
+        
+        if opcao == "1":
+            cadastro()
+            menu()
+
+        elif opcao == "2":
+            lista() 
+            menu()
+
+        elif opcao == "3":   
+            alterar_produto()
+            menu()
+
+        elif opcao == "4":
+            excluir()
+            menu()
+
+        elif opcao == "sair" :
+            print("saindo...")   
+            break   
+
+
+def cadastro():   
+    lista = []
+    nome = input ("digite o nome do produto: ")
+    lista.append(nome)
+
+    with open('concessionaria.txt', 'w') as arquivo: 
+        arquivo.writelines(lista)
+        
+
+def lista():
+    lista = []
+    with open('concessionaria.txt', 'r') as arquivo: 
+            lista = arquivo.readlines()
+            print(lista)
+    
+
+def alterar_produto():
+    produto_alterar = input("Digite o nome do carro para alterar:\n").strip()
+    
+    with open('concessionaria.txt', 'r') as arquivo:
+        lista = arquivo.readlines()
+
+    nova_lista = []                                                                                      PRODUTO = CARRO
+
+    for linha in lista:
+        if linha.strip() == produto_alterar:
+            novo_nome = input("Digite o novo nome do produto:\n").strip()
+            nova_lista.append(novo_nome )
+
+    with open('concessionaria.txt', 'w') as arquivo:
+        arquivo.writelines(nova_lista)
+        print("Produto alterado com sucesso!")
+    
+    
+def excluir():
+    produto_excluir = input("Digite o nome do produto para excluir:\n").strip()
+    
+    with open('concessionaria.txt', 'r') as arquivo:
+        lista = arquivo.readlines()
+
+    nova_lista = []
+
+    for linha in lista:
+        if linha.strip() != produto_excluir:
+            nova_lista.append(linha)
+
+    with open('concessionaria.txt', 'w') as arquivo:
+        arquivo.writelines(nova_lista)
+        print("Produto excluído com sucesso!")
+    
+
+
+menu()
+
+
+
+
+
 
 
                              #EXERCICIO 3
