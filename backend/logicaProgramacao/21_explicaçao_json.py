a4088759432bf2da05ce128290b6098688bf7539
vitoria pineli <vitoriapineli121@gmail.com>
2025-10-07 09:47:56 -0300

    concluindo json

--- backend/logicaProgramacao/21_explicaçao_json.py
+++ backend/logicaProgramacao/21_explicaçao_json.py
@@ -61,23 +61,58 @@ except FileNotFoundError:
 
 
 produto_encontrado = False 
-For produto in inventario: 
-    if produto ["id"] = = id_produto_modificar
+for produto in inventario: 
+    if produto["id"] == id_produto_modificar:
          #colocamos a nova quantidade
         produto ["quantidade"] = nova_quantidade
         produto [" em_estoque"] = nova_quantidade > 0
         produto_encontrado = True
         break;
-
 if not produto_encontrado:
     print (" o produto com o id informado nao foi encontrado")
 
 else:
-    with open ("loja.json", "w") as arquivo
-    json.dump (inventario, arquivo, indent = 4 )
+    with open ("loja.json", "w") as arquivo:
+        json.dump (inventario, arquivo, indent = 4 )
     print ("o arquivo foi alterado com sucesso!!")
 
 
 
-
-
+#xcluir produtos no json 
+try:
+    with open ("loja.json", "r") as arquivo:
+        inventario = json.load(arquivo)
+except FileNotFoundError:
+    print ("arquivo nao encontrado")
+novo_inventario = [ ]
+produto_excluido = False
+id_produto_excluir = int (input("digite o id do produto para excluir"))
+for produto in inventario:
+    if produto ["D"] != id_produto_excluir:
+     #se o id for diferente, adicionamos a nova lista 
+     novo_inventario.append (produto)
+else:
+    print ("produto removido com sucesso!!")
+    produto_excluido = True
+if not produto_excluido:
+    print ("o id do produto nao foi encontrado")
+else:
+    with open ("loja.json", "W") as arquivo:
+        json.dump (novo_inventario, arquivo, indent = 4)
+    print ("o arquivo foi atualizado, prouto removido")
+#listar produtos do arquivo json 
+try:
+    with open ("loja.json", "r") as arquivo:
+        inventario = json.load (arquivo)
+    if not inventario:
+        print ("o arquivo esta vazio!")
+    else:
+        print ("____ lista de produtos no inventario ______")
+        for produto in inventario:
+            print (f"w __ produto {produto.get("id")}___")
+            print (f"Nome:{produto.get("nome_produto","n/a")}")
+            print (f"Preco:{produto.get("preco_unitario", 0):.2f}")
+            print (f"Quantidade:{produto.get("quantidade", 0 )}unidades")
+            print (f"Em estoque:{produto.get("em_estoque")}")
+except FileNotFoundError:
+    print ("arquivo nao encontrado")
