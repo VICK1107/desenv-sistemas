cc5d0ae246892aba60f7d2678dd61b19a4057c26
vitoria pineli <vitoriapineli121@gmail.com>
2025-10-21 09:49:14 -0300

    continuando django

--- gerenciador_tarefas/config/tarefas/urls.py
+++ gerenciador_tarefas/config/tarefas/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path ("", views.listar_tarefas, name = "lista_tarefas"),
+    #detalhes da tarefa 
+    #<int:tarefa_id> captura um numero do url
+    path('<int:tarefa_id>/', views.detalhe_tarefa, name = 'detalhe_tarefa'),
 ]
